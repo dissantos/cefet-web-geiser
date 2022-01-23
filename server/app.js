9837b33a0addea3127322abc3a42189232334f26
dissantos <disantosg18@gmail.com>
2022-01-22 21:12:16 -0300

    Add exercise 1: Initial server configs

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 // importação de dependência(s)
-
+import express from 'express'
+import path from 'path';
 
 // variáveis globais deste módulo
 const PORT = 3000
 const db = {}
+const app = express()
+const __dirname = path.resolve();
 
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
@@ -37,7 +40,10 @@ const db = {}
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
-
+app.use(express.static(__dirname+'/client'));
 
 // abrir servidor na porta 3000 (constante PORT)
 // dica: 1-3 linhas de código
+app.listen(PORT, () => {
+    console.log("Server in port "+ PORT);
+});
