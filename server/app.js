7fbc91d7e511f570182d8ebd3ef1680392df2c94
dissantos <disantosg18@gmail.com>
2022-01-24 21:23:09 -0300

    Add exercise 3: player page

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import path from 'path';
 import fs from 'fs';
+import _ from 'underscore';
 
 // variáveis globais deste módulo
 const PORT = 3000
@@ -29,10 +30,10 @@ app.set('views', 'server/views');
 // dados do banco de dados "data/jogadores.json" com a lista de jogadores
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json (~3 linhas)
-db['players'] = JSON.parse(fs.readFileSync('server/data/jogadores.json'))['players']
+db['players'] = JSON.parse(fs.readFileSync('server/data/jogadores.json'))['players'];
 app.get('/', (req, res) => {
     res.render('index', {players: db['players']})
-})
+});
 
 
 // EXERCÍCIO 3
@@ -40,7 +41,26 @@ app.get('/', (req, res) => {
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter ~15 linhas de código
+db['gamesForPlayers'] = JSON.parse(fs.readFileSync('server/data/jogosPorJogador.json'));
+app.get('/jogador/:numero_identificador/', (req, res) => {
+    const player = _.find(db['players'], (el) => {
+        return req.params.numero_identificador === el.steamid;
+    })
+    const gamesForPlayer = db['gamesForPlayers'][req.params.numero_identificador]['games'];
+    const topGames = _.sortBy(gamesForPlayer, (el) => {
+        return -el['playtime_forever'];
+    })
+    const top5Games = _.first(topGames, 5).map(value => {
+        value['hours_played'] = Math.floor(value['playtime_forever']/60);
+        return value;
+    });
+    const gamesCount = db['gamesForPlayers'][req.params.numero_identificador]['game_count'];
+    const dontPlayGames = _.countBy(gamesForPlayer, (el) => {
+        return el.playtime_forever === 0;
+    })['true'];
 
+    res.render('jogador', { player: player, topGame: top5Games[0], top5Games: top5Games, gamesCount: gamesCount, dontPlayGames: dontPlayGames});
+})
 
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
